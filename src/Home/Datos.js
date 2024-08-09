f7d3a920d15bdf99194352468680eb8ba9a272fa
Andres Lopez <andres.lopez7890@gmail.com>
2024-08-09 12:11:58 -0500

    DataTable

--- src/Home/Datos.js
+++ src/Home/Datos.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { faker } from "@faker-js/faker";
+import { faker, fakerKA_GE } from "@faker-js/faker";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 function Datos() {
@@ -13,6 +13,10 @@ function Datos() {
         fullName: faker.person.fullName(),
         sex: faker.person.sex(),
         id: faker.person.bio(),
+        jobArea: faker.person.jobArea(),
+        prefix: faker.person.prefix(),
+        jobTitle: faker.person.jobTitle(),
+        jobType: faker.person.jobType(),
       });
     }
     setInitData(valuesData);
@@ -21,19 +25,26 @@ function Datos() {
   return (
     <DataTable
       value={initData}
+      scrollable
+      scrollHeight="400px"
       tableStyle={{ minWidth: "10rem" }}
+      selectionMode="single"
       name="table"
       id="table"
-      stripedRows
       paginator
       rows={5}
-      rowsPerPageOptions={[5, 10, 25, 50]}
+      rowsPerPageOptions={[5, 10]}
     >
-      <Column field="firstName" header="Nombre"></Column>
-      <Column field="lastName" header="Apellido"></Column>
+      <Column field="firstName" header="Nombre" sortable></Column>
+      <Column field="lastName" header="Apellido" sortable></Column>
       <Column field="fullName" header="Todo el nombre"></Column>
       <Column field="sex" header="Genero"></Column>
       <Column field="id" header="Actividades"></Column>
+      <Column field="jobArea" header="Area laboral"></Column>
+      <Column field="prefix" header="Prefijo"></Column>
+      <Column field="jobTitle" header="Titulo"></Column>
+      <Column field="jobType" header="Tipo laboral"></Column>
+      <Column field="jobType" header="Tipo laboral"></Column>
     </DataTable>
   );
 }
